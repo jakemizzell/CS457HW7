b8f1ca2d71c09a5655438113779e3e19d17fd196
jbmizzell1 <jakemizzell@gmail.com>
2018-04-10 10:25:16 -0500

    1NF might not work

--- jbmizzell1.c
+++ jbmizzell1.c
@@ -7,57 +7,71 @@ int testForm(char*,char*,char*,FILE*,int);
 int checkEqual(char*,char*,int,int);
 
 int main(void) {
-    int flag = 0;
+    int i;
     int form = 0;
-    FILE *fp;
-    fp = fopen("HW7in.txt", "r");
+    FILE *fp = fopen("HW7in.txt", "r");
     //error message
     if (fp == NULL) {
-        printf("Error in opening file.\n");
+        printf("Error in opening input file.\n");
+        exit(1);
+    }
+    FILE *output = fopen("HW7jbmizzell1.txt", "w");
+    if (output == NULL) {
+        printf("Error in opening output file.\n");
         exit(1);
     }
     //set up 3 arrays to hold stuff
-    char db[15] = {0};
-    char key[10] = {0};
-    char candidate[10] = {0};
+    char db[100] = {0};
+    char key[100] = {0};
+    char candidate[100] = {0};
     int c = getc(fp);
     while(c != EOF) {
-        if (flag == 0) {
-            getDB(db, fp, c);
-            c = getc(fp);
-            getKeys(key, candidate, fp, c);
-            flag = 1;
-            c = getc(fp);
-        }
+        getDB(db, fp, c);
+        c = getc(fp);
+        getKeys(key, candidate, fp, c);
+        c = getc(fp);
         if(c == ';')
             break;
         form = testForm(db,key,candidate,fp,c);
-        if(form == 3)
+        for(i = 0; i < sizeof(db); i++) {
+            db[i] = 0;
+            key[i] = 0;
+            candidate[i] = 0;
+        }
+        if(form == 3) {
             printf("3NF\n");
-        else if (form == 2)
+            fprintf(output,"3NF\n");
+        }
+        else if (form == 2) {
             printf("2NF\n");
-        else
-            printf("1NF\n");
-        flag = 0;
+            fprintf(output,"2NF\n");
+        }
+        else {
+            printf("1NF\n");\
+            fprintf(output,"1NF\n");
+        }
         c = getc(fp);
         //read up anything until the next DB or EOF
         while((c < 64 || c > 91) && c != EOF)
             c = getc(fp);
-        //printf("d%c\n",c);
-        //break;
     }
-    //print out the stuff
     fclose(fp);
-    printf("\n");
+    fclose(output);
     return 0;
 }
 
 int testForm(char *db, char *key, char *can, FILE *fp, int c) {
+    //if there is a candidate key then probably a partial dependency, just set 1NF and read the rest of the stuff
+    if(can[0] != 0) {
+        while(c != 'x')
+            c = getc(fp);
+        return 1;
+    }
     int i;
     int test1 = 0;
     int test2 = 0;
-    char temp1[15] = {0};
-    char temp2[15] = {0};
+    char temp1[100] = {0};
+    char temp2[100] = {0};
     int form = 3;
     while(c != 'x') {
         //eat up past the first paretheses
@@ -72,11 +86,11 @@ int testForm(char *db, char *key, char *can, FILE *fp, int c) {
             temp1[i] = c;
             c = getc(fp);
         }
-        for(i = 0; temp1[i] != 0; i++)
-            printf("%c ",temp1[i]);
+        /*for(i = 0; temp1[i] != 0; i++)
+            printf("%c ",temp1[i]);*/
         //test if key is a subset of temp
         test1 = checkEqual(temp1, key, sizeof(temp1), sizeof(key));
-        printf("Equal1: %d\n", test1);
+        //printf("Equal1: %d  ", test1);
         while (c != '(')
             c = getc(fp);
         c = getc(fp);
@@ -85,14 +99,14 @@ int testForm(char *db, char *key, char *can, FILE *fp, int c) {
             temp2[i] = c;
             c = getc(fp);
         }
-        for(i = 0; temp2[i] != 0; i++)
-            printf("%c ",temp2[i]);
+        /*for(i = 0; temp2[i] != 0; i++)
+            printf("%c ",temp2[i]);*/
         //test if key is a subset of temp
         test2 = checkEqual(temp2, key, sizeof(temp2), sizeof(key));
-        printf("Equal2: %d\n", test2);
+        //printf("Equal2: %d  ", test2);
         if (test1 == 0 && test2 == 0)
             form = 2;
-        printf("Form: %d\n", form);
+        //printf("Form: %d\n", form);
         c = getc(fp);
     }
     return form;
@@ -115,9 +129,9 @@ int checkEqual(char *a1,char *a2 , int m, int n) {
 void getDB(char *db, FILE *fp, int c) {
     for (int i = 0; c != EOF; i++) {
         if(c == ' ') {
-            printf("DB: ");
+            /*printf("DB: ");
             for(i = 0; db[i] != 0; i++)
-                printf("%c",db[i]);
+                printf("%c",db[i]);*/
             return;
         }
         db[i] = c;
@@ -130,13 +144,13 @@ void getKeys(char *key, char *candidate, FILE *fp, int c) {
     for (int i = 0; c != EOF; i++) {
         //thats the end of line
         if(c == ';') {
-            printf("\nKey: ");
+            /*printf("\nKey: ");
             for(i = 0; key[i] != 0; i++)
                 printf("%c",key[i]);
             printf("\nCandidate: ");
             for(i = 0; i < 3; i++)
                 printf("%c",candidate[i]);
-            printf("\n");
+            printf("\n");*/
             return;
         }
         //if there's a space there is now a candidate key to get
